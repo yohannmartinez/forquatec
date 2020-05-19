c8ac961e908ed72cd6f427b13bb39ad5b402feca
yohannmartinez <yohannmartinez34@gmail.com>
2020-05-19 02:08:21 +0200

    test

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const s3 = require("./routes/api/s3");
 const path = require("path");
 const app = express();
 
+app.use(express.static("client/build"));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
@@ -41,11 +47,7 @@ app.use("/api/users", users);
 app.use("/api/articles", articles);
 app.use("/api/s3", s3);
 
-app.use(express.static("client/build"));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
 
 const port = process.env.PORT || 5000;
 
